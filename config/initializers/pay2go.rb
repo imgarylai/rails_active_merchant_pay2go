f33637a91d5fae49b813ffe439256a523737bb5e
Gary <garylai1990@gmail.com>
2015-11-29 00:33:01 +0800

    Init pay2go

---
+++ config/initializers/pay2go.rb
@@ -0,0 +1,6 @@
+OffsitePayments::Integrations::Pay2go.setup do |pay2go|
+  # You have to apply credential below by yourself.
+  pay2go.merchant_id = '31028437'
+  pay2go.hash_key    = 'GADlNOKdHiTBjdgW6uAjngF9ItT6nCW4'
+  pay2go.hash_iv     = 'dzq1naf5t8HMmXIs'
+end
